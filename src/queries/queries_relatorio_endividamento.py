1b2f3c53562b3dd9ff248c915cd661b9ccfbe741
Luiz <luiz.neves9@gmail.com>
2026-09-18 12:17:53 -0300

    feat: atualização da query de endividamento para receber os dados de pagamentos antecipados.

--- src/queries/queries_relatorio_endividamento.py
+++ src/queries/queries_relatorio_endividamento.py
@@ -1,10 +1,27 @@
 SELECT_PROJECAO_ENDIVIDAMENTO = '''
-    SELECT
-        EXTRACT(YEAR from data_vcto) AS ano,
-        EXTRACT(MONTH from data_vcto) AS mes,
-        'Financiamento' AS categoria,
-        ROUND(SUM(total_parcela) / 1000000, 3) AS valor
-    FROM financiamento.vw_agrupamento_projecao
-    GROUP BY EXTRACT(YEAR from data_vcto), EXTRACT(MONTH from data_vcto)
-    ORDER BY EXTRACT(YEAR from data_vcto), EXTRACT(MONTH from data_vcto)
+    WITH valor_endividamento AS (
+        SELECT
+            EXTRACT(YEAR from data_vcto) AS ano,
+            EXTRACT(MONTH from data_vcto) AS mes,
+            'Financiamento' AS categoria,
+            ROUND(SUM(total_parcela) / 1000000, 3) AS valor
+        FROM financiamento.vw_agrupamento_projecao
+        GROUP BY EXTRACT(YEAR from data_vcto), EXTRACT(MONTH from data_vcto)
+        UNION ALL
+        SELECT
+            EXTRACT(YEAR from data_pagamento) AS ano,
+            EXTRACT(MONTH from data_pagamento) AS mes,
+            'Financiamento' AS categoria,
+            ROUND(SUM(valor_pago) / 1000000, 3) AS valor
+        FROM financiamento.antecipacao
+        GROUP BY EXTRACT(YEAR FROM data_pagamento), EXTRACT(MONTH FROM data_pagamento)
+        )
+        SELECT
+            ano,
+            mes,
+            categoria,
+            SUM(valor) AS valor
+        FROM valor_endividamento
+        GROUP BY ano, mes, categoria
+        ORDER BY ano, mes, categoria
 '''
